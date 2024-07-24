79c6c20a6273091c579d367c5e69c255ad776725
aash1407 <aashritha1407@gmail.com>
2024-07-24 14:01:42 +0200

    Fix: duplicate keys error for checkbox session state, clearing input after adding todo

--- web.py
+++ web.py
@@ -8,17 +8,18 @@ def add_todo():
     new_todo = st.session_state["new_todo"] + "\n"
     todos.append(new_todo)
     functions.write_todos(todos)
+    st.session_state["new_todo"] = ""  # Clear input field after adding the todo
 
 
 st.title("My Todo App")
 st.subheader("This is my Todo App")
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+    checkbox = st.checkbox(todo, key=f"{todo}_{index}")
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
-        del st.session_state[todo]
+        del st.session_state[f"{todo}_{index}"]
         st.rerun()
 
 st.text_input(label="todo",label_visibility= "hidden",  placeholder="Add a new Todo task",
